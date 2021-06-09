3ec7658fd660f485b34eb40d77c130d4af3c2f8a
ali <amobarez00@gmail.com>
2021-06-09 13:40:30 +0430

    add dashboard and order routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { history } from '../src/helpers';
 import { Login } from './components/login'
+import { Dashboard } from './components/main/dashboard'
+import { AddOrder } from './components/base/addOrder'
+import PrivateRoute from './components/privateRoute';
 import { Redirect, Route, Router, Switch } from 'react-router';
 
 
@@ -9,6 +12,8 @@ function App() {
         <Router history={history}>
             <Switch>
             <Route exact path="/" component={Login} />
+            <PrivateRoute path="/dashboard" component={Dashboard} ></PrivateRoute>
+            <PrivateRoute path="/addOrder" component={AddOrder} ></PrivateRoute>
             <Redirect from="*" to="/" />
             </Switch>
         </Router>
