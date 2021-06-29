822acac758fabcb2d5828cb87989cab4ec3c8602
ali <amobarez00@gmail.com>
2021-06-29 11:41:09 +0430

    add employees list

--- src/components/employee/employees.js
+++ src/components/employee/employees.js
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Button } from 'react-bootstrap'
 import { Header } from '../base/employeeHeader'
 import { useDispatch, useSelector } from 'react-redux'
 import { employeeActions } from '../../actions/employeeActions'
+import { Row, Button, Container, Card } from 'react-bootstrap'
+import moment from 'jalali-moment';
+import persianJs from 'persianjs/persian.min';
 
 import { AddEmployee } from './addEmployee'
 
-export const Employees = () => {
-    const [modalShow, setModalShow] = useState(false)
+import editIcon from '../../assets/images/Products/edit.svg'
 
+export const Employees = () => {
+    const [addModalShow, setAddModalShow] = useState(false)
+    const [editModalShow, setEditModalShow] = useState(false)
+    const [employee, setEmployee] = useState({})
+    
     let employees = useSelector(state => state.getEmployees.employees)
     const dispatch = useDispatch()
-
     useEffect(() => {
         dispatch(employeeActions.getEmployees())
     }, [dispatch])
 
     return (
-        <>
-            <Header title="کارمندان" backLink="/dashboard" setModalShow={setModalShow} />
-            <Row className="m-0">
-                <span>کارمندان شما</span>
-            </Row>
-
-            <AddEmployee show={modalShow} onHide={() => setModalShow(false)} />
-        </>
+        <div className="product-page">
+            <Header title="کارمندان" backLink="/dashboard" setModalShow={setAddModalShow} />
+            <Container className="m-auto">
+                {employees ? 
+                    (employees.map((item, index) => 
+                        <Card key={index} className="m-auto mt-3 bg-light productCard" >
+                            <Card.Body className="pb-0 ps-1 rounded-3">
+        
+                                <Card.Text className="pt-1">
+                                    <span style={{"color": "var(--text-color-one)"}}>نام : </span>{persianJs(item.family).englishNumber().toString()}
+                                </Card.Text>
+                                <Card.Text className="pt-1">
+                                    <span style={{"color": "var(--text-color-one)"}}>شماره تماس : </span>{persianJs(item.mobile).englishNumber().toString()} تومان
+                                </Card.Text>
+                                
+                                <Card.Link className="editLogo w-100 d-block m-auto" onClick={() => {setEditModalShow(true); setEmployee(item)}}>
+                                    <img className="d-block me-auto" src={editIcon} height="42px" alt="back-icon" />
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>    
+                    ))    
+                    
+                    : null}
+            </Container>
+            <AddEmployee show={addModalShow} onHide={() => setEditModalShow(false)} />
+        </div>
     )
 }
