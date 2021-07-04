d3beb09ee850d96ceaf940e7b391ee547db175bf
root <root@localhost.localdomain>
2021-07-04 14:30:29 +0430

    merg

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@
 
 // export const SERVER_URL = "http://127.0.0.1:3000/api/user/v1";
 
-export const SERVER_URL = "http://crm-x.ir:3000/api/user/v1";
+export const SERVER_URL = "http://www.crm-x.ir:3000/api/user/v1";
