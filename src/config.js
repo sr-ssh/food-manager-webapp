affc41ef3e98b36f18b9319d08ff4071d8823488
reyhaneh <r.sadatshokouhi@gmail.com>
2021-07-18 09:24:00 +0430

    merge

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //export const SERVER_URL = "http://192.168.1.145:3000/api/user/v1";
 
-export const SERVER_URL = "http://192.168.1.127:3000/api/user/v1";
+// export const SERVER_URL = "http://192.168.1.127:3000/api/user/v1";
 
 // export const SERVER_URL = "http://127.0.0.1:3000/api/user/v1";
-// export const SERVER_URL = "http://www.crm-x.ir:3000/api/user/v1";
+export const SERVER_URL = "http://www.crm-x.ir:3000/api/user/v1";
 
