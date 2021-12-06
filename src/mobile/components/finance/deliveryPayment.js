06be1d4b64e88e7b7fbdf65ddfc4375c29a2f25b
saeid niyabaty <saeidniyabaty@gmail.com>
2021-12-06 18:51:55 +0330

    fixing card

--- src/mobile/components/finance/deliveryPayment.js
+++ src/mobile/components/finance/deliveryPayment.js
@@ -7,7 +7,29 @@ export const DeliveryPayment = () => {
        <>
        <div className="deliveryPayment">
            <div className="cards cards__deliveryPayment">
-               5
+               <div className="deliveryPayment__text">
+                 <span className="deliveryPayment__text-name">نام:</span>
+                 <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
+                 <span className="deliveryPayment__text-charge">شارژ:</span>
+                 <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
+               </div>
+               <div className="deliveryPayment__text">
+                 <span className="deliveryPayment__text-mobile">موبایل:</span>
+                 <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
+               </div>
+               <div className="deliveryPayment__text">
+                 <span className="deliveryPayment__text-iban">شماره شبا:</span>
+                 <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
+               </div>
+               <div className="deliveryPayment__text">
+                 <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
+                 <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
+               </div>
+               <div className="deliveryPayment__text">
+                 <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
+                 <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
+               </div>
+               <a className="btn btn__deliveryPayment">پرداخت</a>
            </div>
            <div className="cards cards__deliveryPayment">
                5
