f6ea00183a284a00e696b3360a579e6c1741052d
saeid niyabaty <saeidniyabaty@gmail.com>
2021-12-09 12:25:46 +0330

    it is not important to say

--- src/mobile/components/finance/deliveryPayment.js
+++ src/mobile/components/finance/deliveryPayment.js
@@ -3,120 +3,120 @@ import React, { useEffect, useState } from "react";
 export const DeliveryPayment = () => {
 
 
-    return(
-       <>
-       <div className="deliveryPayment">
-           <div className="cards cards__deliveryPayment">
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-name">نام:</span>
-                 <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
-                 <span className="deliveryPayment__text-charge">شارژ:</span>
-                 <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-mobile">موبایل:</span>
-                 <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-iban">شماره شبا:</span>
-                 <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
-                 <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
-                 <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
-               </div>
-               <div className="deliveryPayment__btn">
-               <a className="btn btn__deliveryPayment">پرداخت</a>
-               </div>
-           </div>
-           <div className="cards cards__deliveryPayment">
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-name">نام:</span>
-                 <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
-                 <span className="deliveryPayment__text-charge">شارژ:</span>
-                 <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-mobile">موبایل:</span>
-                 <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-iban">شماره شبا:</span>
-                 <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
-                 <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
-                 <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
-               </div>
-               <div className="deliveryPayment__btn">
-               <a className="btn btn__deliveryPayment">پرداخت</a>
-               </div>
-               
-           </div>
-           <div className="cards cards__deliveryPayment">
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-name">نام:</span>
-                 <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
-                 <span className="deliveryPayment__text-charge">شارژ:</span>
-                 <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-mobile">موبایل:</span>
-                 <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-iban">شماره شبا:</span>
-                 <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
-                 <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
-                 <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
-               </div>
-               <div className="deliveryPayment__btn">
-               <a className="btn btn__deliveryPayment">پرداخت</a>
-               </div>
-           </div>
-           <div className="cards cards__deliveryPayment">
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-name">نام:</span>
-                 <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
-                 <span className="deliveryPayment__text-charge">شارژ:</span>
-                 <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-mobile">موبایل:</span>
-                 <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-iban">شماره شبا:</span>
-                 <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
-                 <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
-               </div>
-               <div className="deliveryPayment__text">
-                 <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
-                 <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
-               </div>
-               <div className="deliveryPayment__btn">
-               <a className="btn btn__deliveryPayment">پرداخت</a>
-               </div>
-           </div>
-       </div>
-       </>
-    )
+  return (
+    <>
+      <div className="deliveryPayment">
+        <div className="cards cards__deliveryPayment">
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-name">نام:</span>
+            <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
+            <span className="deliveryPayment__text-charge">شارژ:</span>
+            <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-mobile">موبایل:</span>
+            <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-iban">شماره شبا:</span>
+            <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
+            <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
+            <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
+          </div>
+          <div className="deliveryPayment__btn">
+            <a className="btn btn__deliveryPayment">پرداخت</a>
+          </div>
+        </div>
+        <div className="cards cards__deliveryPayment">
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-name">نام:</span>
+            <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
+            <span className="deliveryPayment__text-charge">شارژ:</span>
+            <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-mobile">موبایل:</span>
+            <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-iban">شماره شبا:</span>
+            <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
+            <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
+            <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
+          </div>
+          <div className="deliveryPayment__btn">
+            <a className="btn btn__deliveryPayment">پرداخت</a>
+          </div>
+
+        </div>
+        <div className="cards cards__deliveryPayment">
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-name">نام:</span>
+            <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
+            <span className="deliveryPayment__text-charge">شارژ:</span>
+            <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-mobile">موبایل:</span>
+            <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-iban">شماره شبا:</span>
+            <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
+            <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
+            <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
+          </div>
+          <div className="deliveryPayment__btn">
+            <a className="btn btn__deliveryPayment">پرداخت</a>
+          </div>
+        </div>
+        <div className="cards cards__deliveryPayment">
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-name">نام:</span>
+            <span className="deliveryPayment__text-name-value">وحید نیابتی</span>
+            <span className="deliveryPayment__text-charge">شارژ:</span>
+            <span className="deliveryPayment__text-charge-value">۶۵۰۰۰۰ <span>تومان</span></span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-mobile">موبایل:</span>
+            <span className="deliveryPayment__text-mobile-value">۰۹۱۵۷۱۸۲۹۰۷</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-iban">شماره شبا:</span>
+            <span className="deliveryPayment__text-iban-value">IR۲۵۴۷۸۵۴۷۸۹۶۵۴۷۸۹۶۵۴۷۸۹۶۵</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-bankAccount">شماره حساب:</span>
+            <span className="deliveryPayment__text-bankAccount-value">۵۴۷۸۸۹۶۵۴۷۸۵۲۱۴</span>
+          </div>
+          <div className="deliveryPayment__text">
+            <span className="deliveryPayment__text-creditCardNumber">شماره کارت:</span>
+            <span className="deliveryPayment__text-creditCardNumber-value">۶۰۳۷۹۹۷۴۳۸۹۵۹۰۶۷</span>
+          </div>
+          <div className="deliveryPayment__btn">
+            <a className="btn btn__deliveryPayment">پرداخت</a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
