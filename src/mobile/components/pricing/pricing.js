a78be1328b437320fc74898cfb771b8b5beb83ef
saeid niyabaty <saeidniyabaty@gmail.com>
2021-12-11 11:16:59 +0330

    adding few inputs and button

--- src/mobile/components/pricing/pricing.js
+++ src/mobile/components/pricing/pricing.js
@@ -44,6 +44,23 @@ export const Pricing = () => {
   return (
     <div className="pricing__edit">
       <Header title="قیمت دهی" backLink="/dashboard" />
+      <div className="pricing__edit-container-input">
+        <p className="pricing__edit-title">ورودی (تومان)</p>
+        <input type="text" id="fname" name="firstname" className="input input__pricing__edit" />
+      </div>
+      <div className="pricing__edit-container-input">
+        <p className="pricing__edit-title">فاصله (متر)</p>
+        <input type="text" id="fname" name="firstname" className="input input__pricing__edit" />
+      </div>
+      <div className="pricing__edit-container-input">
+        <p className="pricing__edit-title">حداقل قیمت (تومان)</p>
+        <input type="text" id="fname" name="firstname" className="input input__pricing__edit" />
+      </div>
+      <div className="pricing__edit-container-input">
+        <p className="pricing__edit-title">زمان مسیر (تومان)</p>
+        <input type="text" id="fname" name="firstname" className="input input__pricing__edit" />
+      </div>
+      <a className="btnn btnn__pricing__edit">ثبت کردن</a>
 
     </div>
   );
